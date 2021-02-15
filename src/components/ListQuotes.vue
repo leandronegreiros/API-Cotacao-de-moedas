2254bd03a27d418970bef6571b36436728bf8f97
leandro <leandronegreiros07@gmail.com>
2021-02-15 14:38:28 -0400

    Listagem das moedas
     Instalação do VueCLI e setup do projeto,
    Instalação e configuração do Spectre.css,
    Instalação e configuração do Axios,
     Criando estados reativos com o Composition API,
    Buscando dados da API com o Axios,
     Criação do componente para listagem das moedas

---
+++ src/components/ListQuotes.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Máximo</th>
+                <th>Mínimo</th>
+                <th>Variação</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(quote, key) in quotes" :key="key">
+                <td>{{ key }}</td>
+                <td>{{ quote.name }}</td>
+                <td>{{ quote.high }}</td>
+                <td>{{ quote.low }}</td>
+                <td>
+                    <span 
+                        class="label label-rounded text-smal"
+                        :class="{ 'label-error': quote.pctchange < 0, 'label-success':   quote.pctchange > 0}"    
+                    >
+                        {{ quote.pctChange }} %
+                    </span>
+                </td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: { quotes: { type: Object, requires: true }}
+}
+</script>
